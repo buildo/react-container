94bc0749367d73132e1c9f73b499d6fc8336121f
Giovanni Gonzaga <giovanni@buildo.io>
2016-06-09 18:40:12 +0200

    make decorator config optional

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import compact from 'lodash/compact';
 import flowRight from 'lodash/flowRight';
 import { t, props } from 'tcomb-react';
 import { skinnable, pure, contains } from 'revenge';
+import connect from 'state/connect';
 import noLoaderLoading from './noLoaderLoading';
 
 const ContainerConfig = t.interface({
@@ -15,47 +16,61 @@ const ContainerConfig = t.interface({
   __DO_NOT_USE_additionalPropTypes: t.maybe(t.dict(t.String, t.Type))
 }, { strict: true, name: 'ContainerConfig' });
 
-export default function({ declareQueries, declareCommands, declareConnect }) {
-  return (Component, config = {}) => {
-    const {
-      connect, queries, commands,
-      loadingDecorator = noLoaderLoading, // force a "safety" loader
-      mapProps,
-      __DO_NOT_USE_additionalPropTypes: __props
-    } = ContainerConfig(config);
+const DecoratorConfig = t.interface({
+  declareConnect: t.maybe(t.Function),
+  declareQueries: t.maybe(t.Function),
+  declareCommands: t.maybe(t.Function)
+}, { strict: true, name: 'DecoratorConfig' });
 
-    const declaredQueries = queries && declareQueries(queries);
-    const declaredCommands = commands && declareCommands(commands);
-    const declaredConnect = connect && declareConnect(connect);
-    const loader = queries && loadingDecorator;
-    const propsTypes = {
-      ...(queries ? declaredQueries.Type : {}),
-      ...(commands ? declaredCommands.Type : {}),
-      ...(connect ? declaredConnect.Type : {}),
-      ...(__props ? __props : {})
-    };
-    const composedDecorators = flowRight(...compact([
-      declaredQueries,
-      declaredCommands,
-      declaredConnect,
-      loader
-    ]));
+const decorator = ({ declareQueries, declareCommands, declareConnect }) => (Component, config = {}) => {
+  const {
+    connect, queries, commands,
+    loadingDecorator = noLoaderLoading, // force a "safety" loader
+    mapProps,
+    __DO_NOT_USE_additionalPropTypes: __props
+  } = ContainerConfig(config);
 
-    const getLocals = mapProps || pick([
-      ...(queries || []),
-      ...(commands || []),
-      ...Object.keys(connect || {})
-    ]);
-
-    @composedDecorators
-    @skinnable(contains(Component))
-    @pure
-    @props(propsTypes)
-    class ContainerFactoryWrapper extends React.Component {
-      static displayName = `${Component.displayName || Component.name || 'Component'}Container`;
-      getLocals = getLocals
-    }
-
-    return ContainerFactoryWrapper;
+  const declaredQueries = queries && declareQueries(queries);
+  const declaredCommands = commands && declareCommands(commands);
+  const declaredConnect = connect && declareConnect(connect);
+  const loader = queries && loadingDecorator;
+  const propsTypes = {
+    ...(queries ? declaredQueries.Type : {}),
+    ...(commands ? declaredCommands.Type : {}),
+    ...(connect ? declaredConnect.Type : {}),
+    ...(__props ? __props : {})
   };
+  const composedDecorators = flowRight(...compact([
+    declaredQueries,
+    declaredCommands,
+    declaredConnect,
+    loader
+  ]));
+
+  const getLocals = mapProps || pick([
+    ...(queries || []),
+    ...(commands || []),
+    ...Object.keys(connect || {})
+  ]);
+
+  @composedDecorators
+  @skinnable(contains(Component))
+  @pure
+  @props(propsTypes)
+  class ContainerFactoryWrapper extends React.Component {
+    static displayName = `${Component.displayName || Component.name || 'Component'}Container`;
+    getLocals = getLocals
+  }
+
+  return ContainerFactoryWrapper;
+};
+
+const defaultWithConnectOnly = decorator(DecoratorConfig({
+  declareConnect: (decl = {}, config = {}) => (
+    connect(decl, { killProps: ['params', 'query', 'router'], ...config })
+  )
+}));
+
+export default function(...args) {
+  return ContainerConfig.is(args[1]) ? defaultWithConnectOnly(...args) : decorator(DecoratorConfig(args[0]))
 }
