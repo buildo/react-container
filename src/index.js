338b02695e2c86f0434675b2a509cc3f11bc8364
Giovanni Gonzaga <giovanni@buildo.io>
2017-01-20 11:57:42 +0100

    POC

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import pick from 'lodash/fp/pick';
 import compact from 'lodash/compact';
 import flowRight from 'lodash/flowRight';
+import mapKeys from 'lodash/mapKeys';
+import mapValues from 'lodash/mapValues';
 import { t, props } from 'tcomb-react';
 import { skinnable, pure, contains } from 'revenge';
 import _declareConnect from 'state/connect';
 import _declareQueries from 'react-avenger/queries';
 import _declareCommands from 'react-avenger/commands';
 import noLoaderLoading from './noLoaderLoading';
-import displayName from './displayName';
+import _displayName from './displayName';
 
 const ContainerConfig = t.interface({
   loadingDecorator: t.maybe(t.Function),
   connect: t.maybe(t.dict(t.String, t.Type)),
+  local: t.maybe(t.dict(t.String, t.Type)),
   queries: t.maybe(t.list(t.String)),
   commands: t.maybe(t.list(t.String)),
   reduceQueryProps: t.maybe(t.Function),
@@ -43,16 +46,31 @@ const defaultDeclareConnect = (decl = {}, config = {}) => (
   _declareConnect(decl, { killProps: ['params', 'query', 'router'], ...config })
 );
 
+let _containerCounter = 0;
+
 const decorator = ({ declareQueries, declareCommands, declareConnect }) => (Component, config = {}) => {
   const {
-    connect, queries, commands,
+    connect, local, queries, commands,
     loadingDecorator = noLoaderLoading, // force a "safety" loader
     reduceQueryProps: reduceQueryPropsFn,
     mapProps,
     __DO_NOT_USE_additionalPropTypes: __props
   } = ContainerConfig(config);
 
-  const declaredQueries = queries && declareQueries(queries);
+  const displayName = _displayName(Component, 'Container');
+
+  _containerCounter += 1; // eslint-disable-line operator-assignment
+  const containerNamespace = `__${displayName}-${_containerCounter}__`;
+  const globalizeLocalKeys = obj => mapKeys(obj, (_, k) => {
+    if (local[k]) {
+      return `${containerNamespace}${k}`;
+    }
+    return k;
+  });
+  const localizeLocalKeys = obj => mapKeys(obj, (_, k) => {
+    return k.replace(containerNamespace, '');
+  });
+  const globalizedLocal = globalizeLocalKeys(local || {});
 
   const reduceQueryPropsDecorator = () => {
     const pickQueries = pick([...(queries || [])]);
@@ -61,7 +79,7 @@ const decorator = ({ declareQueries, declareCommands, declareConnect }) => (Comp
     return Component => (
       class ReduceQueryPropsWrapper extends React.Component {
 
-        static displayName = displayName(Component, 'reduceQueryProps');
+        static displayName = _displayName(Component, 'reduceQueryProps');
 
         state = {};
 
@@ -84,15 +102,20 @@ const decorator = ({ declareQueries, declareCommands, declareConnect }) => (Comp
     );
   };
 
+  const declaredQueries = queries && declareQueries(queries);
   const reduceQueryProps = queries && reduceQueryPropsFn && reduceQueryPropsDecorator();
   const declaredCommands = commands && declareCommands(commands);
-  const declaredConnect = connect && declareConnect(connect);
+  const declaredConnect = (connect || local) && declareConnect({
+    ...(connect || {}),
+    ...((local && globalizedLocal) || {})
+  });
   const loader = queries && loadingDecorator;
   const propsTypes = {
     ...(__props ? __props : {}),
     ...(queries ? declaredQueries.Type : {}),
     ...(commands ? declaredCommands.Type : {}),
-    ...(connect ? declaredConnect.Type : {})
+    ...(connect ? declaredConnect.Type : {}),
+    ...(local ? { ...globalizedLocal, transition: t.Function } : {})
   };
   const composedDecorators = flowRight(...compact([
     declaredQueries,
@@ -102,19 +125,42 @@ const decorator = ({ declareQueries, declareCommands, declareConnect }) => (Comp
     loader
   ]));
 
-  const getLocals = mapProps || pick([
+  const getLocals = mapProps || localizeLocalKeys(pick([
     ...(queries || []),
     ...(commands || []),
-    ...Object.keys(connect || {})
-  ]);
+    ...Object.keys({ ...(connect || {}), ...globalizedLocal })
+  ]));
 
   @composedDecorators
   @skinnable(contains(Component))
   @pure
   @props(propsTypes)
   class ContainerFactoryWrapper extends React.Component { // eslint-disable-line react/no-multi-comp
-    static displayName = displayName(Component, 'Container');
-    getLocals = getLocals;
+    static displayName = displayName;
+    containerNamespace = containerNamespace;
+    getLocals = props => {
+      if (props.transition && local) {
+        // TODO: this only supports the "object" form of `transition`
+        // not the "function" one
+        return getLocals(localizeLocalKeys({
+          ...props,
+          transition: obj => {
+            if (t.Object.is(obj)) {
+              return props.transition(globalizeLocalKeys(obj));
+            }
+            return props.transition(obj);
+          }
+        }));
+      }
+      return getLocals(props);
+    }
+    componentWillUnmount = () => {
+      if (local) {
+        setTimeout(() => {
+          this.props.transition(mapValues(globalizedLocal, () => null));
+        });
+      }
+    }
   }
 
   return ContainerFactoryWrapper;
